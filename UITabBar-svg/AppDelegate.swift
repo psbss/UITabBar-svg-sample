b63ee397fea745890c90254da5edb74e634c6dfe
yuu <uypsbss@gmail.com>
2021-05-10 16:54:40 +0900

    init

---
+++ UITabBar-svg/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  UITabBar-svg
+//
+//  Created by yuehara on 2021/05/10.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        
+        return true
+    }
+
+}
+
---
+++ UITabBar-svg/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITabBar-svg
+//
+//  Created by yuehara on 2021/05/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
